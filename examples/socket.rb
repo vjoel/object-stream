819cf2d9c174879098d0f8dac59aa1329a8652db
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-04-28 17:36:25 -0700

    add socket example

---
+++ examples/socket.rb
@@ -0,0 +1,52 @@
+case ARGV[0]
+when "marshal", "yaml", "json", "msgpack"
+else
+  abort "Usage: #$0 marshal|yaml|json|msgpack"
+end
+
+require 'object-stream'
+require 'tmpdir'
+require 'socket'
+
+begin
+  type = ARGV.shift.intern
+  dir = Dir.mktmpdir "stream-"
+
+  s, t = UNIXSocket.pair
+
+  pid = fork do
+    stream = ObjectStream.new(s, type: type)
+    10.times do |i|
+      stream << i
+      if i == 5
+        puts "simulating a slow sender"
+        sleep 2
+        puts "sender is continuing"
+      end
+    end
+  end
+
+
+  s.close
+  stream = ObjectStream.new(t, type: type)
+
+  puts "try to select"
+  until stream.eof? # Otherwise, EOFError is raised at end.
+    select([stream]) # Note stream usable as IO.
+    
+    # Use #read instead of #each or #map so that, in the case of msgpack and
+    # yajl, only the available bytes in io are copied to the stream's buffer
+    # and parsed to objects.
+    stream.read do |obj|
+      p obj
+    end
+
+    puts "select again"
+  end
+  puts "t.eof? = #{t.eof?}"
+  stream.close
+
+ensure
+  FileUtils.remove_entry dir
+  Process.wait pid if pid
+end
