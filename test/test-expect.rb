35f0bf1aa3bcc98d6a1c99a1be220cba1db8b87c
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-04-30 15:30:37 -0700

    add test for expect

---
+++ test/test-expect.rb
@@ -0,0 +1,80 @@
+require 'object-stream'
+require 'stringio'
+
+require 'minitest/autorun'
+
+class TestExpect < MiniTest::Unit::TestCase
+  attr_reader :sio, :stream
+  
+  class A
+    def initialize x, y
+      @x, @y = x, y
+    end
+    
+    def to_msgpack pk = nil
+      case pk
+      when MessagePack::Packer
+        pk.write_array_header(2)
+        pk.write @x
+        pk.write @y
+        return pk
+      
+      else # nil or IO
+        MessagePack.pack(self, pk)
+      end
+    end
+    
+    def to_a
+      [@x, @y]
+    end
+    
+    def to_json
+      to_a.to_json
+    end
+    
+    def self.from_serialized ary
+      new *ary
+    end
+    
+    def == other
+      self.class == other.class and
+        to_a == other.to_a
+    end
+  end
+  
+  class B < A
+  end
+
+  def setup
+    @sio = StringIO.new
+  end
+  
+  def test_expect
+    objects = []
+    20.times do |i|
+      if rand < 0.5
+        objects << "A" << A.new(i, i.to_s)
+      else
+        objects << "B" << B.new(i, i.to_s)
+      end
+    end
+    
+    stream = ObjectStream.new(sio, type: ObjectStream::MSGPACK_TYPE)
+    objects.each do |object|
+      stream << object
+    end
+    
+    sio.rewind
+    stream = ObjectStream.new(sio, type: ObjectStream::MSGPACK_TYPE)
+    objects2 = []
+    stream.read do |object|
+      case object
+      when "A"; stream.expect A
+      when "B"; stream.expect B
+      else stream.unexpect
+      end
+      objects2 << object
+    end
+    assert_equal objects, objects2
+  end
+end
